1187042552d0cdf721026f7583aa49287b1ba267
Fyodor <mitcovfn@gmail.com>
2016-11-21 21:27:01 +0200

    add class UserInterface.java

---
+++ src/mfn/chess/UserInterface.java
@@ -0,0 +1,14 @@
+package mfn.chess;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Fyodor
+ */
+public class UserInterface extends JPanel{
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+    }
+}
