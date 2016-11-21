8aa46b25a392986fddd7bd79ae83e8eb08ebf745
Fyodor <mitcovfn@gmail.com>
2016-11-21 22:45:47 +0200

    draw Chess Board

--- src/mfn/chess/UserInterface.java
+++ src/mfn/chess/UserInterface.java
@@ -6,9 +6,27 @@ import java.awt.*;
 /**
  * Created by Fyodor
  */
-public class UserInterface extends JPanel{
+class UserInterface extends JPanel {
+
+    private static final int CELL_WIDTH = 64;
+    private static final int CELL_HEIGHT = 64;
+
     @Override
-    public void paintComponents(Graphics g) {
-        super.paintComponents(g);
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawBoard(g);
+    }
+
+    private void drawBoard(Graphics g) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++)
+                if ((i + j) % 2 == 0) {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(i * CELL_WIDTH, j * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
+                } else {
+                    g.setColor(Color.LIGHT_GRAY);
+                    g.fillRect(i * CELL_WIDTH, j * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
+                }
+        }
     }
 }
