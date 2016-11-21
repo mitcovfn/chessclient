c8c3507852a149a41f010f3277f9a342fc23616a
Fyodor <mitcovfn@gmail.com>
2016-11-21 22:43:11 +0200

    add icon, frame location to centre

--- src/mfn/chess/Chess.java
+++ src/mfn/chess/Chess.java
@@ -2,17 +2,27 @@ package mfn.chess;
 
 
 import javax.swing.*;
+import java.awt.*;
 
+/**
+ * Created by Fyodor
+ */
 public class Chess {
-    public static final int WIDTH = 500;
-    public static final int HEIGHT = 500;
+    private static final int WINDOW_WIDTH = 519;
+    private static final int WINDOW_HEIGHT = 542;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Chess");
-        UserInterface ui = new UserInterface();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(WIDTH, HEIGHT);
+        UserInterface ui = new UserInterface();
         frame.add(ui);
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (dim.width - WINDOW_WIDTH) / 2;
+        int y = (dim.height - WINDOW_HEIGHT) / 2;
+        frame.setLocation(x, y);
+        ImageIcon icon = new ImageIcon("img/icon.jpg");
+        frame.setIconImage(icon.getImage());
         frame.setResizable(false);
         frame.setVisible(true);
     }
