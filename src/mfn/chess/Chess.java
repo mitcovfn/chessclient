6b0211a847992036e83e6c5335642c42f7255ab9
Fyodor <mitcovfn@gmail.com>
2016-11-22 22:20:41 +0200

    add matrix chessBoard

--- src/mfn/chess/Chess.java
+++ src/mfn/chess/Chess.java
@@ -11,6 +11,17 @@ public class Chess {
     private static final int WINDOW_WIDTH = 519;
     private static final int WINDOW_HEIGHT = 542;
 
+    static String[][] chessBoard = {
+            {"r", "k", "b", "q", "a", "b", "k", "r"},
+            {"p", "p", "p", "p", "p", "p", "p", "p"},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {"P", "P", "P", "P", "P", "P", "P", "P"},
+            {"R", "K", "B", "Q", "A", "B", "K", "R"},
+    };
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Chess");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
