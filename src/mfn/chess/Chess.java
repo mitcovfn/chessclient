b3b8352fb4e70430cce8893b1dfc5b4b0fb6f1c3
Fyodor <mitcovfn@gmail.com>
2016-11-28 17:54:14 +0200

    add engine movement to Knight (Horse)

--- src/mfn/chess/Chess.java
+++ src/mfn/chess/Chess.java
@@ -19,7 +19,7 @@ public class Chess {
             {" ", " ", " ", " ", " ", " ", " ", " "},
             {" ", " ", " ", " ", " ", " ", " ", " "},
             {" ", " ", " ", " ", " ", " ", " ", " "},
-            {"R", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
             {"P", "P", "P", "P", "P", "P", "P", "P"},
             {"R", "K", "B", "Q", "A", "B", "K", "R"},
     };
@@ -129,9 +129,40 @@ public class Chess {
         }
         return list;
     }
-
+    //Knight movement (Horse)
     public static String posibleK(int i) {
-        String list = "";
+        String list = "", oldPiece;
+        int r = i / 8, c = i % 8;
+        for (int j = -1; j <= 1; j+=2) {
+            for (int k = -1; k <= 1; k+=2) {
+                try {
+                    if (Character.isLowerCase(chessBoard[r + j][c + k * 2].charAt(0)) ||" ".equals(chessBoard[r + j][c + k * 2])) {
+                        oldPiece = chessBoard[r + j][c + k * 2];
+                        chessBoard[r][c]=" ";
+                        //chessBoard[r + j][c + k * 2] ="K";
+                        if (kingSafe()) {
+                            list = list + r + c + (r + j)+ (c + k * 2) + oldPiece;
+                        }
+                        chessBoard[r][c]="K";
+                        chessBoard[r + j][c + k * 2] =oldPiece;
+                    }
+                } catch (Exception e) {   }
+
+                try {
+                    if (Character.isLowerCase(chessBoard[r + j * 2][c + k].charAt(0)) ||" ".equals(chessBoard[r + j * 2][c + k])) {
+                        oldPiece = chessBoard[r + j * 2][c + k];
+                        chessBoard[r][c]=" ";
+                        //chessBoard[r + j][c + k * 2] ="K";
+                        if (kingSafe()) {
+                            list = list + r + c + (r + j * 2)+ (c + k) + oldPiece;
+                        }
+                        chessBoard[r][c]="K";
+                        chessBoard[r + j * 2][c + k] =oldPiece;
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }
         return list;
     }
     //Bishop Movement
